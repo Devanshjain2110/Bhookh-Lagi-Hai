2225ec85168827176a96754507e89f685f05fc43
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2023-08-13 21:34:17 +0530

    Changed icon

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,18 @@
+import { Link } from 'react-router-dom';
 import Gen from '../items/Gen.png'
 
 const Title = () => (
     <div className="">
+      <Link to="/">
       <img 
       data-testid="logo"
       className="w-28 px-4 pt-2 ml-6"
       alt="logo"
     src={Gen}
+   
     
       />
+       </Link>
     </div>
 
     
