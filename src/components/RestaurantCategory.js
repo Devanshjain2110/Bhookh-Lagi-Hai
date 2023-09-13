0e4f507fd4153ea3a2b1f0b3bbef911307b4cfc6
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2023-09-13 09:07:22 +0530

    Made cursor hover on categoties in menu

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -9,9 +9,9 @@ export const RestaurantCategory = ({ data }) => {
   
   return (
     <>
-      <div className="collapse collapse-arrow bg-white text-black max-w-9xl collapse-close" onClick={handleClick}>
-        <input type="radio" name="my-accordion-2" />
-        <div className="collapse-title text-xl font-medium shadow-xl mb-[3]" >
+      <div className="collapse collapse-arrow bg-white text-black max-w-9xl collapse-close " onClick={handleClick}>
+        <input type="radio" name="my-accordion-2" className="hover:cursor-pointer"/>
+        <div className="collapse-title text-xl font-medium shadow-xl mb-[3] " >
           {data.title} ({data.itemCards.length})
         </div>
         <div className="collapse-content">
